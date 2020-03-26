a6ddbe757830e01aca8c1073525ceecf26fd3ea2
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-03-26 13:59:17 -0400

    Add super basic component unit tests.

--- src/components/app/__tests__/App.test.tsx
+++ src/components/app/__tests__/App.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from '../App';
 
-test('passes', () => {
-  expect(true);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
--- src/components/stimuli/stimuliCard/__tests__/StimuliCard.test.tsx
+++ src/components/stimuli/stimuliCard/__tests__/StimuliCard.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Stimuli from '../Stimuli';
+import StimuliCard from '../StimuliCard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Stimuli />, div);
+  ReactDOM.render(<StimuliCard url="someurl.com" />, div);
 });
