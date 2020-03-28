f1d280d7fa1a7135145111988ff05583f768eb69
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-03-28 12:54:50 -0400

    Add enzyme and unit tests for StimuliCard and Home

--- src/components/home/__tests__/Home.test.tsx
+++ src/components/home/__tests__/Home.test.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Home from '../Home';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
+describe('<Home />', () => {
+  it('renders without crashing', () => {
+    shallow(<Home />);
+  });
+
+  it('renders four buttons', () => {
+    const wrapper = shallow(<Home />);
+    const button = wrapper.find('Button');
+    expect(button).toHaveLength(4);
+  });
+
+  it('renders the button to run experiments', () => {
+    const wrapper = shallow(<Home />);
+    const button = wrapper.find('Button').at(0);
+    expect(button.text()).toEqual('Run Experiment');
+  });
+
+  it('renders the button to manage logs', () => {
+    const wrapper = shallow(<Home />);
+    const button = wrapper.find('Button').at(1);
+    expect(button.text()).toEqual('Manage Logs');
+  });
+
+  it('renders the button to manage experiments', () => {
+    const wrapper = shallow(<Home />);
+    const button = wrapper.find('Button').at(2);
+    expect(button.text()).toEqual('Manage Experiments');
+  });
+
+  it('renders the button to manage stimuli', () => {
+    const wrapper = shallow(<Home />);
+    const button = wrapper.find('Button').at(3);
+    expect(button.prop('href')).toEqual('/stimuli');
+    expect(button.text()).toEqual('Manage Stimuli');
+  });
 });
--- src/components/stimuli/__tests__/Stimuli.test.tsx
+++ src/components/stimuli/__tests__/Stimuli.test.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import fetchMock from 'jest-fetch-mock';
 import Stimuli from '../Stimuli';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Stimuli />, div);
+describe('<Stimuli />', () => {
+  it('renders without crashing', async () => {
+    await shallow(<Stimuli />);
+  });
+
+  it.skip('renders an error when fetching stimuli fails', async () => {
+    const mockResponse = {
+      files: [
+        'http://localhost/static/img/some_image_url.jpg',
+        'http://localhost/static/img/some_other_image_url.jpg',
+      ],
+    };
+    fetchMock.mockRejectOnce(new Error('Some error'));
+    const wrapper = await shallow(<Stimuli />);
+    const errorDiv = wrapper.find('Row').find('div');
+    expect(fetchMock.mock.calls.length).toEqual(1);
+    expect(errorDiv).toHaveLength(1);
+    expect(errorDiv.text()).toEqual('Error encountered while loading images.');
+  });
 });
