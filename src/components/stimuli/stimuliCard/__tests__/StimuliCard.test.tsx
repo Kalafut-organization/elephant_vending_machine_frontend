a89dbe025200e8cc496b0d5b7cf341c00f8b193a
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-04-19 15:27:18 -0400

    Add delete button to stimuli cards (#20)

--- src/components/stimuli/stimuliCard/__tests__/StimuliCard.test.tsx
+++ src/components/stimuli/stimuliCard/__tests__/StimuliCard.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import Modal from 'react-bootstrap/Modal';
 import StimuliCard from '../StimuliCard';
 
 describe('<StimuliCard />', () => {
@@ -23,13 +25,16 @@ describe('<StimuliCard />', () => {
     expect(wrapper.contains('unknown filename')).toEqual(true);
   });
 
-  it('renders a view button', () => {
+  it('renders view and delete buttons and hidden modal buttons', () => {
     const wrapper = shallow(
       <StimuliCard url="http://localhost/static/img/some_image_url.jpg" />
     );
     const button = wrapper.find('Button');
-    expect(button).toHaveLength(1);
-    expect(button.text()).toEqual('View');
+    expect(button).toHaveLength(4);
+    expect(button.at(0).text()).toEqual('View');
+    expect(button.at(1).text()).toEqual('Delete');
+    expect(button.at(2).text()).toEqual('Cancel');
+    expect(button.at(3).text()).toEqual('Delete');
   });
 
   it('renders an image preview for the url passed as prop', () => {
@@ -41,5 +46,89 @@ describe('<StimuliCard />', () => {
     expect(image.prop('src')).toEqual(
       'http://localhost/static/img/some_image_url.jpg'
     );
+  });
+
+  it('renders a model when the delete button is clicked.', async () => {
+    const wrapper = shallow(
+      <StimuliCard url="http://localhost/static/img/some_stimuli_url.py" />
+    );
+    await act(async () => {
+      wrapper
+        .find('Button')
+        .at(1)
+        .simulate('click');
+    });
+    expect(wrapper.find(Modal).props().show).toBe(true);
+  });
+
+  it('sends delete API call when delete then confirm buttons are pressed', async () => {
+    const mockResponse = {
+      message: ['mock message'],
+    };
+    const fetchMock = jest.spyOn(global, 'fetch').mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockResponse),
+      })
+    );
+    const windowReloadMock = jest
+      .spyOn(window.location, 'reload')
+      .mockImplementation(() => {});
+
+    const wrapper = shallow(
+      <StimuliCard url="http://localhost/static/experiment/some_experiment_url.py" />
+    );
+    await act(async () => {
+      wrapper
+        .find('Button')
+        .at(1)
+        .simulate('click');
+    });
+    await act(async () => {
+      wrapper
+        .find('Button')
+        .at(3)
+        .simulate('click');
+    });
+    expect(fetchMock).toHaveBeenCalled();
+    expect(windowReloadMock).toHaveBeenCalled();
+    fetchMock.mockRestore();
+    windowReloadMock.mockRestore();
+  });
+
+  it('closes the modal when you click delete and then cancel', async () => {
+    const wrapper = shallow(
+      <StimuliCard url="http://localhost/static/experiment/some_experiment_url.py" />
+    );
+    await act(async () => {
+      wrapper
+        .find('Button')
+        .at(1)
+        .simulate('click');
+    });
+    expect(wrapper.find(Modal).props().show).toBe(true);
+    await act(async () => {
+      wrapper
+        .find('Button')
+        .at(2)
+        .simulate('click');
+    });
+    expect(wrapper.find(Modal).props().show).toBe(false);
+  });
+
+  it('hides the modal when the modals hide action is triggered.', async () => {
+    const wrapper = shallow(
+      <StimuliCard url="http://localhost/static/experiment/some_experiment_url.py" />
+    );
+    await act(async () => {
+      wrapper
+        .find('Button')
+        .at(1)
+        .simulate('click');
+    });
+    expect(wrapper.find(Modal).props().show).toBe(true);
+    await act(async () => {
+      wrapper.find(Modal).simulate('hide');
+    });
+    expect(wrapper.find(Modal).props().show).toBe(false);
   });
 });
