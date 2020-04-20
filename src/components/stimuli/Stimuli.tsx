ac1ce1a015a128718d3cb9e71a8b0727b1522466
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-04-19 23:31:37 -0400

    Remove unused imports

--- src/components/stimuli/Stimuli.tsx
+++ src/components/stimuli/Stimuli.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import Spinner from 'react-bootstrap/Spinner';
 import Toast from 'react-bootstrap/Toast';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import StimuliCard from './stimuliCard';
 
 const generateCards = (stimuliUrls: Array<string>): Array<JSX.Element> => {
